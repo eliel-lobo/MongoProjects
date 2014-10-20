ee62b24495bc175d2dd59a06f237655416c97d0d
Eliel Lobo <eliel.lobo@ceiba.com.co>
2014-10-20 16:44:25 -0500

    Spark + Freemarker + mongo

--- HelloProject/src/main/java/co/com/mongo/HelloSpark.java
+++ HelloProject/src/main/java/co/com/mongo/HelloSpark.java
@@ -1,16 +1,9 @@
 package co.com.mongo;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
-
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class HelloSpark {
 	
@@ -25,29 +18,7 @@ public class HelloSpark {
 			}
 		});
 		
-		Spark.get(new Route("/HelloFreemarker") {
-			
-			@Override
-			public Object handle(Request arg0, Response arg1) {
-				
-				Configuration configuration = new Configuration();
-				configuration.setClassForTemplateLoading(HelloSpark.class, "/");
-				StringWriter writer = new StringWriter();
-				
-				try {
-					Template template = configuration.getTemplate("hello.ftl");
-					
-					Map<String, Object> map = new HashMap<String, Object>();
-					map.put("message", "Hello from freemarker");
-					
-					template.process(map, writer);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				return writer.toString();
-			}
-		});
+		
 	}
 	
 }
